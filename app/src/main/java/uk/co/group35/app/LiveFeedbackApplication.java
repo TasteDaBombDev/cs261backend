69e82995dd2321001d71c6c8962123c73fff5d64
Rares Dragutu <raresdragutu9@gmail.com>
2021-02-17 21:52:11 +0200

    FRONT PAGE LINKS CREATED

--- app/src/main/java/uk/co/group35/app/LiveFeedbackApplication.java
+++ app/src/main/java/uk/co/group35/app/LiveFeedbackApplication.java
@@ -14,3 +14,32 @@ public class LiveFeedbackApplication {
     }
 
 }
+/*
+
+login /api/user/login - POSTGRESQL
+register /api/user/register - POSTGRESQL
+
+--USERPAGE--
+
+fetch past host events /api/events/past/{userid} - POSTGRESQL
+list all events that the user will go /api/events/attending/{userid} - POSTGRESQL
+accept/deny the events (join event for public) /api/events/invites/{userid} - POSTGRESQL
+list all public events that they have not register (filters) /api/events/list - POSTGRESQL
+
+see previous events (host only) + analytics /api/events/display/{eventid} - MONGO
+joins events /api/event/join/{eventid},{userid} - POSTGRESQL (check if the user is attending to that particular event)
+
+--EVENT_CREATION--
+
+create event /api/event/create/{userid,eventName,eventStartDate,eventStopDate,questions + template type} - POSTGRESQL,MONGO - Sync
+list all the users /api/users/list - POSTGRESQL
+
+--LIVE_Feedback_FORM-
+
+submit a feedback /api/submit/{userid},{eventid} - MONGO
+
+--LIVE_EVENT_PAGE-
+fetch_data for the event /api/events/data/{eventid} - MONGO
+
+
+*/
--- app/src/main/java/uk/co/group35/app/events/enums/FormTemplates.java
+++ app/src/main/java/uk/co/group35/app/events/enums/FormTemplates.java
@@ -1,5 +1,7 @@
 package uk.co.group35.app.events.enums;
 
+// TO DO: Add a string for the question
+
 public enum FormTemplates {
     SLIDER_FORM,
     STEPS_FORM,
